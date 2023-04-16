bc40f8d89587c6633bfdb581bed266db0f5af8c7
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2023-04-16 08:54:26 +0200

    For home page changes

--- Home_Page.php
+++ Home_Page.php
@@ -1,36 +1,7 @@
-<<<<<<< HEAD
 <?php 
     $page_title = "Home Page";
     include('includes/Header.php'); 
     include('includes/Nav-bar.php'); 
 ?>
-
-<div class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h4>Login And Registration</h4>
-            </div>
-        </div>
-    </div>
-</div>
-
-=======
-<?php 
-    $page_title = "Home Page";
-    include('includes/Header.php'); 
-    include('includes/Nav-bar.php'); 
-?>
-
-<div class="py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h4>Login And Registration</h4>
-            </div>
-        </div>
-    </div>
-</div>
-
->>>>>>> 246a15c096a35ade5f083b3e0c02ceefe2eca41b
+<div></div>
 <?php include('includes/Footer.php'); ?>
--- includes/Header.php
+++ includes/Header.php
@@ -8,6 +8,5 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="travel.css" rel="stylesheet">
         <script src="https://kit.fontawesome.com/2566f27170.js" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
