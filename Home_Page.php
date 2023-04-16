46d73f201ae2d1e87645952283319a89772f0cee
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2023-04-16 11:04:29 +0200

    Updated home page

--- Home_Page.php
+++ Home_Page.php
@@ -3,5 +3,16 @@
     include('includes/Header.php'); 
     include('includes/Nav-bar.php'); 
 ?>
-<div></div>
+
+<section class="intro">
+    <div class="intro-text">
+        <h1>Welcome to the Finnest Travel Site in Zambia.</h1>
+        <p>
+            We offer services from taking you from different places in Zambia to
+            hosting you in the best hotels and logdes in our beautiful country,
+            Scroll down for more info on our offers. 
+        </p>
+    </div>
+</section>
+
 <?php include('includes/Footer.php'); ?>
--- includes/Nav-Bar.php
+++ includes/Nav-Bar.php
@@ -1,3 +1,14 @@
 <header>
-  
+  <a href="Home_Page.php" class="logo">Finess</a>
+
+  <ul class="navlist">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Travel</a></li>
+    <li><a href="#">Places</a></li>
+    <li><a href="#">Packages</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Login</a></li>
+  </ul>
+
+  <div class="bx bx-menu" id="menu-icon"></div>
 </header>
