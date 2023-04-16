37ac1337402805b55c48415944b2aef002c0767f
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2023-04-16 09:25:12 +0200

    updaetes and new travel.js file

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,4 +1,5 @@
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ></script>
+        <!-- Custom JS  -->
+        <script src="travel.js"></script>
     </body>
 </html>
--- includes/Header.php
+++ includes/Header.php
@@ -6,7 +6,22 @@
         </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Custom-CSS  -->
         <link href="travel.css" rel="stylesheet">
+
+        <!-- BoxIcons  -->
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+        <!-- RemixIcons  -->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+
+        <!-- FontAwesomeIcons  -->
         <script src="https://kit.fontawesome.com/2566f27170.js" crossorigin="anonymous"></script>
+
+        <!-- googleFonts  -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Poppins:ital,wght@0,200;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet">
     </head>
     <body>
