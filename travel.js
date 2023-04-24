8125dfa217debb7c3d3f1454b522d195b27ec310
williamsiuluta <williamjsiuluta@gmail.com>
2023-04-24 03:09:36 -0700

    corrected some wrong spellings

---
+++ travel.js
@@ -0,0 +1,18 @@
+//myscript.js file
+function myUniqueFunction() {
+    // Your JavaScript code here 
+}
+
+//main.html or main.php file 
+<html>
+<head>
+<script src="myscript.js"></script>
+<style>
+    /* your CSS code here */
+</style>
+
+</head>
+<body>
+!-- your HMTL code here--;
+</body>
+</html>
