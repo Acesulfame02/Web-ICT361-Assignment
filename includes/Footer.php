efe47211b5826494f3f10c45d07c8e14417b81e4
Aaron Masembe <aaronmasembemujabikalibala@gmail.com>
2023-04-25 06:22:25 +0200

    Added footer to page

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,4 +1,61 @@
+        <footer>
+            <div class="main-content">
+                <div class="left box">
+                    <h2>About Us</h2>
+                    <div class="content">
+                        <p>
+                            Welcome to our website, where we aim to provide you with the best travel experiences possible. Our team of travel experts has years of experience in the industry and is dedicated to creating unforgettable trips for our clients. Whether you're looking for a relaxing beach getaway or an adventurous mountain trek, we have the perfect package for you.
 
+                            We pride ourselves on our attention to detail and personalized service. Our team will work with you to create a custom itinerary that meets your specific needs and preferences. We believe that travel should be more than just a vacation - it should be an opportunity to learn, explore, and grow.
+
+                            In addition to our travel packages, we also offer a community for our members to connect and share their experiences. Our cost-sharing program allows members to split the costs of travel and accommodation, making it more affordable for everyone. We encourage our members to leave reviews and ratings for our tours and lodging facilities, so that others can benefit from their experiences.
+
+                            At our core, we are passionate travelers who believe that everyone should have the opportunity to explore the world. We hope that our website inspires you to take the first step towards your next adventure. Thank you for choosing us as your travel partner.
+                        </p>
+                        <div class="social">
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="center">
+                    <h2>Address</h2>
+                    <div class="content">
+                        <div class="place">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <i class="text">Chinsali District.</i>
+                        </div>
+                        <div class="phone">
+                            <i class="fa-solid fa-phone"></i>
+                            <i class="text">+2609876541</i>
+                        </div>
+                        <div class="email">
+                            <i class="fa-regular fa-envelope"></i>
+                            <i class="text">finness@gmail.com</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="right box">
+                    <h2>Contact Us</h2>
+                    <div class="content">
+                        <form action="#">
+                            <div class="email">
+                                <div class="text">Email *</div>
+                                <input type="email" required>
+                            </div>
+                            <div class="msg">
+                                <div class="text">Message *</div>
+                                <textarea rows="2" cols="25" required></textarea>
+                            </div>
+                            <div class="btn">
+                                <button type="submit">send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </footer>
         <!-- Custom JS  -->
         <script src="travel.js"></script>
     </body>
