de6eb2fc9eb5cfe050bcef21497148a799314e70
RuthMwape <ruthmwape20@gmail.com>
2023-05-11 12:38:53 -0700

    more detail.

--- profile_form.php
+++ profile_form.php
@@ -22,7 +22,23 @@
     <div class="contenter">
         <h1 id="page-title">Welcome <?php echo $username; ?> back to Our website</h1>
         <div id="page-content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,     dignissim     sit amet, adipiscing nec, ultricies sed, dolor.</p>
+          <p>Our travel website welcomes you! Your user profile is the key to personalized travel experiences and seamless trip planning. Whether you're a seasoned traveler or embarking on your first adventure, we're here to make your journey unforgettable.</p>
+          <br>
+          <p>
+             With your user profile, you can easily manage travel preferences, save favorite destinations, and access exclusive deals tailored just for you. Our platform offers a wealth of travel resources, including destination guides, insider tips, and customer reviews, to help you make informed decisions and discover hidden gems worldwide.
+          </p>
+          <br>
+          <p>
+          By sharing your preferences and interests, we can provide personalized recommendations that match your unique travel style. Whether you crave a relaxing beach escape, thrilling adventures, cultural immersion, or culinary exploration, we have you covered.
+          </p>
+          <br>
+          <p>
+          Your user profile also allows you to track bookings, view itineraries, and communicate with our dedicated support team for assistance. We're committed to ensuring your travel experience is smooth and hassle-free.
+          </p>
+          <br>
+          <p>
+          Take a moment to set up your user profile and unlock a world of personalized travel possibilities. Let us be your trusted companion on your journey as you create lifelong memories. Happy travels!
+          </p>
         </div>
     </div>
 </div>
